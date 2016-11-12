4e8d24e0cf44bbf660accba2d3db937a26e1fa17
Neo Sepulveda <israel.sepulveda.castillejo@gmail.com>
2016-11-12 12:24:20 +0000

    CRUD React tutorial ready

---
+++ app/assets/javascripts/components/event_application.js.jsx
@@ -0,0 +1,86 @@
+var EventApplication = React.createClass({
+  getInitialState: function() {
+    return { events: [],
+             sort: "name",
+             order: "asc" };
+  },
+  componentDidMount: function() {
+    this.getDataFromApi();
+  },
+  getDataFromApi: function() {
+    var self = this;
+    $.ajax({
+      url: '/api/events',
+      success: function(data) {
+        self.setState({ events: data });
+      },
+      error: function(xhr, status, error) {
+        alert('Cannot get data from API: ', error);
+      }
+    });
+  },
+  handleSearch: function(events) {
+    this.setState({ events: events });
+  },
+  handleAdd: function(event) {
+    var events = this.state.events;
+    events.push(event);
+    this.setState({ events: events });
+  },
+  handleDeleteRecord: function(event) {
+    var events = this.state.events.slice();
+    var index = events.indexOf(event);
+    events.splice(index, 1);
+    this.setState({ events: events });
+  },
+  handleUpdateRecord: function(old_event, event) {
+    var events = this.state.events.slice();
+    var index = events.indexOf(old_event);
+    events.splice(index, 1, event);
+    this.setState({ events: events });
+  },
+  handleSortColumn: function(name, order) {
+    if (this.state.sort != name) {
+      order = 'asc';
+    }
+    $.ajax({
+      url: '/api/events',
+      data: { sort_by: name, order: order },
+      method: 'GET',
+      success: function(data) {
+        this.setState({ events: data, sort: name, order: order });
+      }.bind(this),
+      error: function(xhr, status, error) {
+        alert('Cannot sort events: ', error);
+      }
+    });
+  },
+  render: function() {
+    return(
+      <div className="container">
+        <div className="jumbotron">
+          <h1>ReactJS Tutorial</h1>
+          <p>by Neo Sepulveda</p>
+        </div>
+        <div className="row">
+          <div className="col-md-4">
+            <SearchForm handleSearch={this.handleSearch} />
+          </div>
+          <div className="col-md-8">
+            <NewForm handleAdd={this.handleAdd} />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12">
+            <EventTable events={this.state.events}
+                        sort={this.state.sort}
+                        order={this.state.order}
+                        handleDeleteRecord={this.handleDeleteRecord}
+                        handleUpdateRecord={this.handleUpdateRecord}
+                        handleSortColumn={this.handleSortColumn} />
+          </div>
+        </div>
+      </div>
+    )
+  }
+});
---
+++ app/controllers/api/events_controller.rb
@@ -0,0 +1,59 @@
+module Api
+  class EventsController < ApplicationController
+    before_action :set_event, only: [:update, :destroy]
+
+    def index
+      render json: Event.order(sort_by + ' ' + order)
+    end
+
+    def create
+      event = Event.new(event_params)
+      if event.save
+        render json: event
+      else
+        render nothing: true, status: :bad_request
+      end
+    end
+
+    def update
+      if @event.update(event_params)
+        render json: @event
+      else
+        render nothing: true, status: :unprocessable_entity
+      end
+    end
+
+    def search
+      query = params[:query]
+      events = Event.where('name LIKE ? OR place LIKE ? OR description LIKE ?',
+                           "%#{query}%", "%#{query}%", "%#{query}%")
+      render json: events
+    end
+
+    def destroy
+      @event.destroy
+      head :no_content
+    end
+
+    private
+
+    def event_params
+      params.require(:event).permit(:name, :description, :event_date, :place)
+    end
+
+    def set_event
+      @event = Event.find(params[:id])
+    end
+
+    def sort_by
+      %w(name
+         place
+         description
+         event_date).include?(params[:sort_by]) ? params[:sort_by] : 'name'
+    end
+
+    def order
+      %w(asc desc).include?(params[:order]) ? params[:order] : 'asc'
+    end
+  end
+end
